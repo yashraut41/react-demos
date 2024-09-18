6a65b2e8b2249c1cb6f9f45f7815f99797c313e3
yashraut41 <yashwantraut41@icloud.com>
2024-09-18 17:45:29 +0530

    moduling

---
+++ hello-world/app.js
@@ -0,0 +1,12 @@
+const heading = React.createElement('h1', { id: 'heading' }, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById('rooot'));
+// root.render(heading);
+
+const parent = React.createElement(
+    'div', { id: 'parent' },
+    React.createElement(
+        'div',
+        { id: 'child' },
+        [React.createElement('h1', {}, 'I am H1 tag'), React.createElement('h2', {}, 'I am H2 tag')]));
+root.render(parent)
+console.log(parent)

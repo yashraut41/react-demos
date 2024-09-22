e5eaed93769e00e4ff4853d8d0fb8f018ab634da
yashraut41 <yashwantraut41@icloud.com>
2024-09-22 19:06:37 +0530

    different ways calling component

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,14 +1,33 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-const heading = React.createElement('h1', { id: 'heading' }, "Hello World from React!");
-const root = ReactDOM.createRoot(document.getElementById('rooot'));
-// root.render(heading);
 
-const parent = React.createElement(
-    'div', { id: 'parent' },
-    React.createElement(
-        'div',
-        { id: 'child' },
-        [React.createElement('h1', {}, 'I am H1 tag'), React.createElement('h2', {}, 'I am H2 tag')]));
-root.render(parent)
-console.log(parent)
+const heading = <h1>Hello world in </h1>
+
+console.log(heading);
+
+
+const Title = () => {
+    return(
+        <h1>I am Title</h1>
+    )
+}
+const HeaderComponent = () => (<h1>I am heder Component</h1>)
+//React Component 
+
+//Functional Component 
+const FunctionalComponent = () => {
+
+    return (
+        <div id="container ">
+            <Title/>
+            <Title></Title>
+            {Title()}
+            <HeaderComponent />
+            <h1>I am Functional Component</h1>
+        </div>
+
+    )
+}
+//jsx
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<FunctionalComponent />)
